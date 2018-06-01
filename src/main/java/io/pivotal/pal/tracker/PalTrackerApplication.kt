48a1aa5f96618a2a812baa3767a651f507f25fd6
Andrew Edstrom <aedstrom@pivotal.io>
2018-06-01 11:05:23 -0700

    Add flyway things to build.gradle
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.kt
@@ -1,7 +1,6 @@
 package io.pivotal.pal.tracker
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonInclude.*
+import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
